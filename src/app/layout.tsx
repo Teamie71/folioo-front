f0ce6a244dc4d93a695568abc558e7411b0ea410
KTAEHWA <80040482+KTAEHWA@users.noreply.github.com>
2025-11-24 16:56:33 +0900

    🎉 프로젝트 세팅

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import "../styles/globals.css";
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko" className={pretendard.className}>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
