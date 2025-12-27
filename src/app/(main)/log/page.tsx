c029ac7c23dfedcd5c1a3aade7c9633d6c539ae2
HyunwooDoo <engusdn0421@gmail.com>
2025-12-27 19:28:24 +0900

    ✨ Feat: 페이지 전체 하단 padding 4.5rem 추가

--- src/app/(main)/log/page.tsx
+++ src/app/(main)/log/page.tsx
@@ -2,7 +2,7 @@ import { Checkbox } from '@/components/ui/checkbox';
 
 export default function LogPage() {
   return (
-    <div className='flex flex-col gap-[4.5rem]'>
+    <div className='flex flex-col gap-[4.5rem] pb-[4.5rem]'>
       {/* 인사이트 로그 헤더 */}
       <div className='mx-auto flex h-[15.625rem] w-full min-w-[66rem] flex-col justify-center bg-[#F6F5FF]'>
         <div className='mx-auto flex min-w-[66rem] flex-col items-start gap-[1rem]'>
