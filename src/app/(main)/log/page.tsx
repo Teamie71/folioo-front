379ca9005212f12faa9028f549f2fc1bfa3ecbaa
HyunwooDoo <engusdn0421@gmail.com>
2025-12-27 22:48:48 +0900

    🎨 Style: svg Stroke 카멜케이스 수정

--- src/app/(main)/log/page.tsx
+++ src/app/(main)/log/page.tsx
@@ -209,14 +209,14 @@ export default function LogPage() {
                   viewBox='0 0 20 20'
                   fill='none'
                 >
-                  <g clip-path='url(#clip0_3242_1984)'>
+                  <g clipPath='url(#clip0_3242_1984)'>
                     <path
                       d='M7.08268 4.58321L10.0289 1.63696L12.9752 4.58321L10.0293 7.52946L7.08268 4.58321ZM12.4993 9.99988L15.4456 7.05363L18.3918 9.99988L15.446 12.9461L12.4993 9.99988ZM1.66602 9.99988L4.61227 7.05363L7.55852 9.99988L4.61268 12.9461L1.66602 9.99988ZM7.08268 15.4165L10.0289 12.4703L12.9752 15.4165L10.0293 18.3628L7.08268 15.4165Z'
                       fill='#74777D'
                       stroke='#74777D'
-                      stroke-width='2'
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
+                      strokeWidth='2'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
                     />
                   </g>
                   <defs>
@@ -293,9 +293,9 @@ export default function LogPage() {
                   <path
                     d='M6 9L12 15L18 9'
                     stroke='black'
-                    stroke-width='2'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
                   />
                 </svg>
               </button>
@@ -318,9 +318,9 @@ export default function LogPage() {
                   <path
                     d='M6 9L12 15L18 9'
                     stroke='black'
-                    stroke-width='2'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
                   />
                 </svg>
               </button>
