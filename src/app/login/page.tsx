076920d727a3f094feff77db781735716f77ac75
HyunwooDoo <engusdn0421@gmail.com>
2025-12-29 15:53:20 +0900

    ✨ Feat: 로그인 페이지 버튼 요소 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,9 +3,9 @@ export default function Login() {
     <div className='mx-auto flex min-h-screen w-full flex-col items-center justify-center gap-[5.5rem]'>
       <div className='flex flex-col items-center justify-center gap-[2rem]'>
         {/* 로고 */}
-        <div className='flex h-[6.25rem] w-[12rem] items-center justify-center bg-[#D9D9D9]'>
+        <button className='flex h-[6.25rem] w-[12rem] cursor-pointer items-center justify-center bg-[#D9D9D9]'>
           로고
-        </div>
+        </button>
 
         {/* 로그인 구분선 */}
         <div className='flex items-center'>
@@ -18,34 +18,34 @@ export default function Login() {
 
         {/* 로그인 버튼 */}
         <div className='flex flex-col gap-[1.25rem]'>
-          <div className='flex cursor-pointer items-center justify-center gap-[0.75rem] rounded-[0.5rem] bg-[#FFCD00] px-[5.625rem] py-[0.75rem] text-[1rem] font-bold text-[#1A1A1A]'>
+          <button className='flex cursor-pointer items-center justify-center gap-[0.75rem] rounded-[0.5rem] bg-[#FFCD00] px-[5.625rem] py-[0.75rem] text-[1rem] font-bold text-[#1A1A1A]'>
             <div className='h-[1.75rem] w-[1.75rem] bg-[#D9D9D9]'>로고</div>
             <span>카카오톡으로 로그인</span>
-          </div>
+          </button>
 
-          <div className='flex cursor-pointer items-center justify-center gap-[0.75rem] rounded-[0.5rem] bg-[#E9EAEC] px-[5.625rem] py-[0.75rem] text-[1rem] font-bold text-[#1A1A1A]'>
+          <button className='flex cursor-pointer items-center justify-center gap-[0.75rem] rounded-[0.5rem] bg-[#E9EAEC] px-[5.625rem] py-[0.75rem] text-[1rem] font-bold text-[#1A1A1A]'>
             <div className='h-[1.75rem] w-[1.75rem] bg-[#D9D9D9]'>로고</div>
             <span>구글로 로그인</span>
-          </div>
+          </button>
 
-          <div className='flex cursor-pointer items-center justify-center gap-[0.75rem] rounded-[0.5rem] bg-[#03C75A] px-[5.625rem] py-[0.75rem] text-[1rem] font-bold text-[#1A1A1A]'>
+          <button className='flex cursor-pointer items-center justify-center gap-[0.75rem] rounded-[0.5rem] bg-[#03C75A] px-[5.625rem] py-[0.75rem] text-[1rem] font-bold text-[#1A1A1A]'>
             <div className='h-[1.75rem] w-[1.75rem] bg-[#D9D9D9]'>로고</div>
             <span className='text-[#FDFDFD]'>네이버로 로그인</span>
-          </div>
+          </button>
         </div>
       </div>
 
       {/* 약관, 개인정보 처리방침, 마케팅 수신 */}
       <div className='flex items-center gap-[6.25rem]'>
-        <div className='cursor-pointer text-[1rem] text-[#000000]'>
+        <button className='cursor-pointer text-[1rem] text-[#000000]'>
           서비스 이용 약관
-        </div>
-        <div className='cursor-pointer text-[1rem] text-[#000000]'>
+        </button>
+        <button className='cursor-pointer text-[1rem] text-[#000000]'>
           개인정보 처리방침
-        </div>
-        <div className='cursor-pointer text-[1rem] text-[#000000]'>
+        </button>
+        <button className='cursor-pointer text-[1rem] text-[#000000]'>
           마케팅 정보 수신
-        </div>
+        </button>
       </div>
     </div>
   );
