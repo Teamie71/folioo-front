b3ca3987c57ff7ee4f76f416e18b4e89bb812740
HyunwooDoo <engusdn0421@gmail.com>
2026-01-29 18:57:13 +0900

    ✨ Feat: 거래 내역 - Backbutton 적용

--- src/app/invoice/page.tsx
+++ src/app/invoice/page.tsx
@@ -1,27 +1,12 @@
 import Link from 'next/link';
+import { BackButton } from '@/components/BackButton';
 
 export default function InvoicePage() {
   return (
     <div className='mx-auto flex w-[66rem] min-w-[66rem] flex-col gap-[3.75rem] pt-[3.75rem]'>
       {/* 헤더 */}
       <div className='flex items-center gap-[1.25rem]'>
-        <button>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='28'
-            height='28'
-            viewBox='0 0 28 28'
-            fill='none'
-          >
-            <path
-              d='M18 23L9 14L18 5.00001'
-              stroke='black'
-              strokeWidth='2.5'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            />
-          </svg>
-        </button>
+        <BackButton />
 
         <span className='text-[1.5rem] font-bold'>크레딧 거래 내역</span>
       </div>
