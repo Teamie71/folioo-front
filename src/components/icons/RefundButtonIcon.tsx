495c95a1895a9d72073edd710cca81b89df00177
HyunwooDoo <engusdn0421@gmail.com>
2026-01-29 19:49:48 +0900

    ✨ Feat: 환불 버튼 아이콘 구현

---
+++ src/components/icons/RefundButtonIcon.tsx
@@ -0,0 +1,19 @@
+export const RefundButtonIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='6'
+      height='11'
+      viewBox='0 0 6 11'
+      fill='none'
+    >
+      <path
+        d='M0.75 9.75L4.75 5.25L0.75 0.75'
+        stroke='#5060C5'
+        stroke-width='1.5'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+    </svg>
+  );
+};
