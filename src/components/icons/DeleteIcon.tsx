744cdc06a16a6c4f93d75d3f0b028a6d0a2d9bb0
HyunwooDoo <engusdn0421@gmail.com>
2026-01-29 21:36:02 +0900

    ✨ Feat: 삭제 아이콘 구현

---
+++ src/components/icons/DeleteIcon.tsx
@@ -0,0 +1,21 @@
+export const DeleteIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='28'
+      height='28'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='#DC0000'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path d='M3 6h18' />
+      <path d='M8 6V4h8v2' />
+      <rect x='5' y='6' width='14' height='15' rx='1' />
+      <path d='M10 11v6' />
+      <path d='M14 11v6' />
+    </svg>
+  );
+};
