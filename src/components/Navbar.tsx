b7503536b4c888cd75ee9f585592a863a5cf8e40
KTAEHWA <80040482+KTAEHWA@users.noreply.github.com>
2026-01-31 18:53:44 +0900

    ✨ Feat: 로그인 버튼 클릭 시 로그인 페이지로 이동

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,15 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const router = useRouter();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogin = () => {
-    setIsLoggedIn((prev) => !prev);
+    setIsLoggedIn(true);
+    router.push('/login');
   };
 
   return (
