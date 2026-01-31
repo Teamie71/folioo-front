8815d045a0f7623be63da76202fa1fa4aa2577db
KTAEHWA <80040482+KTAEHWA@users.noreply.github.com>
2026-01-31 12:18:43 +0900

    🐛 Fix: JD 토글 디자인 수정

--- src/components/ToggleSmall.tsx
+++ src/components/ToggleSmall.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import * as React from 'react';
+import { cn } from '@/utils/utils';
 import { Toggle, ToggleProps } from './ui/CustomToggle';
 
 export function ToggleSmall<T extends string>({
@@ -14,13 +15,17 @@ export function ToggleSmall<T extends string>({
   };
 
   return (
-    <Toggle.Root value={value} onChange={handleChange} className={className}>
+    <Toggle.Root
+      value={value}
+      onChange={handleChange}
+      className={cn('gap-[2px] rounded-[0.25rem] bg-[#E9EAEC]', className)}
+    >
       {options.map((option) => (
         <Toggle.Item
           key={option.value}
           value={option.value}
           activeClassName="bg-[#5060C5] text-[#FFFFFF]"
-          inactiveClassName="bg-[#E9EAEC] text-[#74777D]"
+          inactiveClassName="bg-transparent text-[#74777D]"
           buttonClassName="h-[29px] w-[77px] rounded-[0.25rem] text-[0.875rem]"
         >
           {option.label}
