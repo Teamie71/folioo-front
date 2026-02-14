87eccd7250fc5e5b0c25945b50409195a3dcdc4e
HyunwooDoo <engusdn0421@gmail.com>
2026-02-14 17:00:23 +0900

    ✨ Feat: 거래 내역 페이지 tanstack table 표 구현

--- src/features/invoice/components/RefundButton.tsx
+++ src/features/invoice/components/RefundButton.tsx
@@ -3,11 +3,12 @@ import Link from 'next/link';
 
 export const RefundButton = () => {
   return (
-    <Link href='/invoice/refund'>
-      <button className='flex w-[5.7rem] cursor-pointer items-center gap-[0.375rem] rounded-[6.25rem] border-[0.09375rem] border-[#5060C5] bg-[#F6F5FF] px-[0.75rem] py-[0.375rem] text-[0.875rem] text-[#5060C5]'>
-        <p>환불 신청</p>
-        <RefundButtonIcon />
-      </button>
+    <Link
+      href='/invoice/refund'
+      className='inline-flex w-[5.7rem] cursor-pointer items-center gap-[0.375rem] rounded-[6.25rem] border-[0.09375rem] border-[#5060C5] bg-[#F6F5FF] px-[0.75rem] py-[0.375rem] text-[0.875rem] text-[#5060C5]'
+    >
+      <span>환불 신청</span>
+      <RefundButtonIcon />
     </Link>
   );
 };
