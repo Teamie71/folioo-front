32989e119bbc19070e32689200942c3228bc7361
HyunwooDoo <engusdn0421@gmail.com>
2026-02-15 16:51:06 +0900

    ✨ Feat: 연관 로그 좌우 아이콘 추가

---
+++ src/components/icons/ChevronColorLeftIcon.tsx
@@ -0,0 +1,19 @@
+export const ChevronColorLeftIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='11'
+      height='25'
+      viewBox='0 0 11 25'
+      fill='none'
+    >
+      <path
+        d='M9.5 1.5L1.5 12.5L9.5 23.5'
+        stroke='#5060C5'
+        strokeWidth='3'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
