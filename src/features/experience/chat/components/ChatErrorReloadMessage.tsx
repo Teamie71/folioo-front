fad5e80d45760284c9c23c80ec4fd7cac2b1b8a1
HyunwooDoo <engusdn0421@gmail.com>
2026-02-15 17:35:27 +0900

    ✨ Feat: 답변 에러 메시지 구현

---
+++ src/features/experience/chat/components/ChatErrorReloadMessage.tsx
@@ -0,0 +1,17 @@
+import { CommonButton } from '@/components/CommonButton';
+
+export const ChatErrorReloadMessage = () => {
+  return (
+    <div className='flex flex-col gap-[1rem]'>
+      <p className='text-[1rem] leading-[160%] text-[#1A1A1A]'>
+        앗, 답변 생성 중 오류가 발생했어요.
+        <br />
+        아래 버튼을 눌러 다시 시도해주세요.
+      </p>
+
+      <CommonButton variantType='Outline' px='2.25rem' py='0.5rem'>
+        다시 시도하기
+      </CommonButton>
+    </div>
+  );
+};
