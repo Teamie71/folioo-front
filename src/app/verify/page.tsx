ba0d1a7e34e9fc12ea65a8591a70aff4b938223c
KTAEHWA <80040482+KTAEHWA@users.noreply.github.com>
2026-02-18 15:25:24 +0900

    🐛 Fix: 휴대폰 인증 페이지 문구 수정

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -33,7 +33,7 @@ export default function VerifyPage() {
               휴대폰 번호 인증
             </h1>
             <p className='text-[1.125rem] text-[#464B53]'>
-              3초만에 회원가입 완료하고, 무료 이용권을 받으세요!
+              3초만에 회원가입 완료하고, 무료 이용권으로 시작하세요!
             </p>
           </div>
 
