df967ad56cd92531e8348e8bb21c9394a0f244a5
KTAEHWA <80040482+KTAEHWA@users.noreply.github.com>
2026-02-20 23:55:56 +0900

    ✨ Feat: 활동 삭제 아이콘 추가

---
+++ src/components/icons/ActivityDeleteIcon.tsx
@@ -0,0 +1,8 @@
+export default function ActivityDeleteIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+      <rect width="14" height="14" rx="7" fill="#74777D" />
+      <line x1="3.75" y1="7.25" x2="10.25" y2="7.25" stroke="white" strokeWidth="1.5" strokeLinecap="round" />
+    </svg>
+  );
+}
