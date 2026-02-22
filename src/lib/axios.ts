61028b7450eb63f7034289196e64aaf17e4705e8
HyunwooDoo <engusdn0421@gmail.com>
2026-02-22 23:07:09 +0900

    ✨ Feat: API 호출 시 실제 데이터만 반환 처리

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
@@ -6,6 +6,18 @@ const axiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+// Orval이 API를 호출할 때 사용할 커스텀 함수 추가
+export const customInstance = <T>(
+  config: AxiosRequestConfig,
+  options?: AxiosRequestConfig,
+): Promise<T> => {
+  return axiosInstance({
+    ...config,
+    ...options,
+  }).then((response) => response.data);
+  // response.data만 반환하도록 처리
+};
 
 export default axiosInstance;
 export { axiosInstance as apiClient };
