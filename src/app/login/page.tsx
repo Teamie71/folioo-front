6dcb8c19e759073698de64e8f6c89476855ce2e6
HyunwooDoo <engusdn0421@gmail.com>
2026-02-22 23:54:54 +0900

    ✨ Feat: 로그인 API 연동

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,39 +1,21 @@
+import { GoogleLoginButton } from '@/features/login/GoogleLoginButton';
+import { KakaoLoginButton } from '@/features/login/KakaoLoginButton';
+import { NaverLoginButton } from '@/features/login/NaverLoginButton';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Login() {
   return (
-    <div className='mx-auto flex min-h-screen w-full flex-col items-center justify-center gap-[5.5rem]'>
-      <div className='flex flex-col items-center justify-center gap-[2rem]'>
+    <div className='mx-auto flex min-h-screen w-full flex-col items-center justify-center gap-[3.75rem]'>
+      <div className='flex flex-col items-center justify-center gap-[3.75rem]'>
         {/* 로고 */}
-        <button className='flex h-[6.25rem] w-[12rem] cursor-pointer items-center justify-center bg-[#D9D9D9]'>
-          로고
-        </button>
-
-        {/* 로그인 구분선 */}
-        <div className='flex items-center'>
-          <p className='w-[11.125rem] border border-[#9EA4A9]' />
-          <div className='px-[1rem] py-[0.625rem] text-center text-[1rem] text-[#74777D]'>
-            로그인
-          </div>
-          <p className='w-[11.125rem] border border-[#9EA4A9]' />
-        </div>
+        <Image src='/LoginLogo.svg' alt='MainLogo' width={260} height={65} />
 
         {/* 로그인 버튼 */}
         <div className='flex flex-col gap-[1.25rem]'>
-          <button className='flex cursor-pointer items-center justify-center gap-[0.75rem] rounded-[0.5rem] bg-[#FFCD00] px-[5.625rem] py-[0.75rem] text-[1rem] font-bold text-[#1A1A1A]'>
-            <div className='h-[1.75rem] w-[1.75rem] bg-[#D9D9D9]'>로고</div>
-            <span>카카오톡으로 로그인</span>
-          </button>
-
-          <button className='flex cursor-pointer items-center justify-center gap-[0.75rem] rounded-[0.5rem] bg-[#E9EAEC] px-[5.625rem] py-[0.75rem] text-[1rem] font-bold text-[#1A1A1A]'>
-            <div className='h-[1.75rem] w-[1.75rem] bg-[#D9D9D9]'>로고</div>
-            <span>구글로 로그인</span>
-          </button>
-
-          <button className='flex cursor-pointer items-center justify-center gap-[0.75rem] rounded-[0.5rem] bg-[#03C75A] px-[5.625rem] py-[0.75rem] text-[1rem] font-bold text-[#1A1A1A]'>
-            <div className='h-[1.75rem] w-[1.75rem] bg-[#D9D9D9]'>로고</div>
-            <span className='text-[#FDFDFD]'>네이버로 로그인</span>
-          </button>
+          <KakaoLoginButton />
+          <NaverLoginButton />
+          <GoogleLoginButton />
         </div>
       </div>
 
