96f7683bf9518d467f193abd9127b0e3641218b0
HyunwooDoo <engusdn0421@gmail.com>
2026-02-28 15:35:54 +0900

    🎨 Design: 템플릿 선택 아이콘, 디자인 수정

--- src/components/SingleButtonGroup.tsx
+++ src/components/SingleButtonGroup.tsx
@@ -34,7 +34,7 @@ export function SingleButtonGroup({
 
   return (
     <>
-      {/* 선택된 상태에서 아이콘 색상 변경 - fill/stroke 속성에 따라 조건부 적용 */}
+      {/* 미선택 아이콘 #74777D, 선택 시 #5060C5 */}
       <style
         dangerouslySetInnerHTML={{
           __html: `
@@ -42,15 +42,19 @@ export function SingleButtonGroup({
             width: auto;
             height: auto;
           }
-          [data-state='on'] span svg path[fill] {
-            fill: #5060C5 !important;
+          [data-state='off'] span svg path[fill],
+          [data-state='off'] span svg g path[fill] {
+            fill: #74777D !important;
           }
-          [data-state='on'] span svg path[stroke] {
-            stroke: #5060C5 !important;
+          [data-state='off'] span svg path[stroke],
+          [data-state='off'] span svg g path[stroke] {
+            stroke: #74777D !important;
           }
+          [data-state='on'] span svg path[fill],
           [data-state='on'] span svg g path[fill] {
             fill: #5060C5 !important;
           }
+          [data-state='on'] span svg path[stroke],
           [data-state='on'] span svg g path[stroke] {
             stroke: #5060C5 !important;
           }
@@ -71,10 +75,10 @@ export function SingleButtonGroup({
               value={option.label}
               className={cn(
                 // 기본 스타일링
-                'font-regular flex cursor-pointer items-center gap-[1rem] rounded-[3.75rem] border-[0.0625rem] border-[#9EA4A9] bg-[#FDFDFD] px-[1.5rem] py-[1.125rem] text-[1rem] text-[#1A1A1A]',
+                'font-regular flex cursor-pointer items-center gap-[1rem] rounded-[3.75rem] border-[0.0625rem] border-[#9EA4A9] bg-[#FDFDFD] px-[1.5rem] py-[0.625rem] text-[1rem] text-[#1A1A1A]',
 
                 // 선택된 상태 효과
-                'data-[state=on]:border-[0.125rem] data-[state=on]:border-[#5060C5] data-[state=on]:bg-[#F6F5FF] data-[state=on]:px-[1.43125rem] data-[state=on]:py-[0.3125rem] data-[state=on]:font-semibold data-[state=on]:text-[#5060C5]',
+                'data-[state=on]:border-[0.125rem] data-[state=on]:border-[#5060C5] data-[state=on]:bg-[#F6F5FF] data-[state=on]:px-[1.4375rem] data-[state=on]:py-[0.5rem] data-[state=on]:font-semibold data-[state=on]:text-[#5060C5]',
               )}
               style={
                 {
