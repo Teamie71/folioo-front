10c27055e3fe7fa217a3df044b0837a0a486ab89
HyunwooDoo <engusdn0421@gmail.com>
2026-03-03 23:29:28 +0900

    ✨ Feat: 로그인 요구 모달 닫기 버튼 제거

--- src/components/LoginRequiredModal.tsx
+++ src/components/LoginRequiredModal.tsx
@@ -1,23 +1,36 @@
 'use client';
 
-import { CommonModal } from '@/components/CommonModal';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/Dialog';
+import { cn } from '@/utils/utils';
 
 interface LoginRequiredModalProps {
   open: boolean;
   onOpenChange: (open: boolean) => void;
 }
 
-export function LoginRequiredModal({
-  open,
-  onOpenChange,
-}: LoginRequiredModalProps) {
+export function LoginRequiredModal({ open }: LoginRequiredModalProps) {
   return (
-    <CommonModal
-      open={open}
-      onOpenChange={onOpenChange}
-      title='로그인이 필요한 서비스입니다.'
-      closeButtonOnly
-      className='w-[23.625rem] px-[5rem] py-[3.75rem]'
-    />
+    <Dialog open={open} onOpenChange={() => {}}>
+      <DialogContent
+        className={cn(
+          'w-[23.625rem] items-center gap-[1.75rem] px-[5rem] py-[3.75rem] text-center',
+          // 닫기 버튼 숨김
+          '[&>button:last-child]:hidden',
+        )}
+        onInteractOutside={(e) => e.preventDefault()}
+        onEscapeKeyDown={(e) => e.preventDefault()}
+      >
+        <DialogHeader className='flex flex-col items-center justify-center gap-[0.25rem] space-y-0'>
+          <DialogTitle className='line-height-[130%] text-center text-[1.125rem] font-bold text-[#1A1A1A]'>
+            로그인이 필요한 서비스입니다.
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
