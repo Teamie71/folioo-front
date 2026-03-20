2484ace06e23dc48a4af4bfff27d02fe3867c970
KTAEHWA <80040482+KTAEHWA@users.noreply.github.com>
2026-03-20 21:42:51 +0900

    ✨ Feat: 로그인 안내 모달 모바일 환경 고려

--- src/components/LoginRequiredModal.tsx
+++ src/components/LoginRequiredModal.tsx
@@ -18,7 +18,7 @@ export function LoginRequiredModal({ open }: LoginRequiredModalProps) {
     <Dialog open={open} onOpenChange={() => {}}>
       <DialogContent
         className={cn(
-          'w-[23.625rem] items-center gap-[1.75rem] px-[5rem] py-[3.75rem] text-center',
+          'w-[calc(100vw-2rem)] max-w-[23.625rem] items-center gap-[1.75rem] px-[1.5rem] py-[2rem] text-center md:w-[23.625rem] md:px-[5rem] md:py-[3.75rem]',
           // 닫기 버튼 숨김
           '[&>button:last-child]:hidden',
         )}
@@ -26,7 +26,7 @@ export function LoginRequiredModal({ open }: LoginRequiredModalProps) {
         onEscapeKeyDown={(e) => e.preventDefault()}
       >
         <DialogHeader className='flex flex-col items-center justify-center gap-[0.25rem] space-y-0'>
-          <DialogTitle className='line-height-[130%] text-center text-[1.125rem] font-bold text-[#1A1A1A]'>
+          <DialogTitle className='line-height-[130%] whitespace-nowrap text-center text-[1rem] font-bold text-[#1A1A1A] md:text-[1.125rem]'>
             로그인이 필요한 서비스입니다.
           </DialogTitle>
         </DialogHeader>
