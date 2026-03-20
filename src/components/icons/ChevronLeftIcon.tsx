a1ee8c9960cd7dbadb31a437ae097c67a35c8759
HyunwooDoo <engusdn0421@gmail.com>
2026-03-20 21:11:32 +0900

    ✨ Feat: 아이콘 크기 조절 추가

--- src/components/icons/ChevronLeftIcon.tsx
+++ src/components/icons/ChevronLeftIcon.tsx
@@ -1,6 +1,7 @@
-export const ChevronLeftIcon = () => {
+export const ChevronLeftIcon = ({ className }: { className?: string }) => {
   return (
     <svg
+      className={className}
       xmlns='http://www.w3.org/2000/svg'
       width='28'
       height='28'
--- src/components/icons/CloseIcon.tsx
+++ src/components/icons/CloseIcon.tsx
@@ -1,6 +1,7 @@
-export const CloseIcon = () => {
+export const CloseIcon = ({ className }: { className?: string }) => {
   return (
     <svg
+      className={className}
       xmlns='http://www.w3.org/2000/svg'
       width='24'
       height='24'
