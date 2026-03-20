5293f3ca29d7b57b70f89ef268b1e9e6238e4163
HyunwooDoo <engusdn0421@gmail.com>
2026-03-20 21:11:06 +0900

    ✨ Feat: 모바일 아이콘 추가

---
+++ src/components/icons/mobile/MobileProfileButtonIcon.tsx
@@ -0,0 +1,19 @@
+export const MobileProfileButtonIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='20'
+      height='20'
+      viewBox='0 0 20 20'
+      fill='none'
+    >
+      <path
+        d='M7.14272 3.57129L13.5713 9.99986L7.14272 16.4284'
+        stroke='black'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
