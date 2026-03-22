9e460dbbffcc90ba8cffcc562fc54ce6ccb4a783
HyunwooDoo <engusdn0421@gmail.com>
2026-03-22 20:10:56 +0900

    🐛 Fix: 모바일 스크롤 불가 현상 수정

--- src/app/(main)/experience/ExperienceClientMobile.tsx
+++ src/app/(main)/experience/ExperienceClientMobile.tsx
@@ -22,7 +22,7 @@ export default function ExperienceClientMobile() {
   }, [isSearching, searchQuery]);
 
   return (
-    <div className='flex min-h-screen flex-col bg-[#FDFDFD]'>
+    <div className='w-full bg-[#FDFDFD]'>
       {/* 경험정리 헤더 (모바일) */}
       <div className='bg-sub1 flex flex-col gap-[0.75rem] px-[1rem] py-[1.25rem]'>
         <p className='typo-c1 text-gray7'>
