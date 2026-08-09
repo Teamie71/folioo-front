7293c26ffedc56570ef9c89bbfb3b85bed252792
KTAEHWA <80040482+KTAEHWA@users.noreply.github.com>
2026-08-09 13:47:28 +0900

    ✨ Feat: 경험정리 페이지 연동

--- src/features/experience/list/components/ExperienceListClient.tsx
+++ src/features/experience/list/components/ExperienceListClient.tsx
@@ -4,20 +4,12 @@ import { ExperienceListSidebar } from '@/features/experience/list/components/Exp
 import { ExperienceListMainPanel } from '@/features/experience/list/components/ExperienceListMainPanel';
 import { ExperienceListAgentPanel } from '@/features/experience/list/components/ExperienceListAgentPanel';
 import { ExperienceListModals } from '@/features/experience/list/components/ExperienceListModals';
-
-function CommonSidebarSlot() {
-  return (
-    <aside
-      className='border-gray3 flex h-full w-[60px] shrink-0 flex-col items-center border-r bg-white pt-[32px]'
-      aria-hidden
-    />
-  );
-}
+import Sidebar from '@/components/Sidebar';
 
 export default function ExperienceListClient() {
   return (
     <div className='flex h-[100dvh] w-full overflow-hidden bg-white'>
-      <CommonSidebarSlot />
+      <Sidebar />
 
       <div className='relative flex min-w-0 flex-1 overflow-hidden'>
         <ExperienceListSidebar />
