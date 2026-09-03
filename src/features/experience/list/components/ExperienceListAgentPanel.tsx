d7913c29a0f5ce1b19b17633da246d74dc4e5455
두현우 <vezz0421@aromit.com>
2026-09-03 21:51:09 +0900

    🐛 Fix: 에이전트 placeholder 수정

--- src/features/experience/list/components/ExperienceListAgentPanel.tsx
+++ src/features/experience/list/components/ExperienceListAgentPanel.tsx
@@ -114,7 +114,7 @@ function AgentComposer({
               onSend();
             }
           }}
-          placeholder={enabled ? '질문하세요.' : 'placeholder'}
+          placeholder='내용 또는 파일을 추가해 주세요.'
           tabIndex={tabIndex}
           className='typo-b2 text-gray9 placeholder:text-gray5 relative z-10 h-full min-w-0 flex-1 bg-transparent pr-[8px] pl-[24px] outline-none disabled:cursor-not-allowed'
         />
